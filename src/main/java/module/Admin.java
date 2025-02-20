5e9b4727ad332b61604b9a653c596743b842e6fa
Sundaresan31 <sundaresanrath2@gmail.com>
2025-02-20 17:45:08 +0530

    Added User's details

---
+++ src/main/java/module/Admin.java
@@ -0,0 +1,11 @@
+package module;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "Admin")
+
+public class Admin extends User{
+}
---
+++ src/main/java/module/Staff.java
@@ -0,0 +1,10 @@
+package module;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Staff")
+
+public class Staff extends User {
+}
