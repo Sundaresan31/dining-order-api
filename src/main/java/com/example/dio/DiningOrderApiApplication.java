ff08842730b661180f2b2c8ef03038afb15af1f9
Sundaresan31 <sundaresanrath2@gmail.com>
2025-02-20 17:22:49 +0530

    Main commit

---
+++ src/main/java/com/example/dio/DiningOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningOrderApiApplication.class, args);
+	}
+
+}
